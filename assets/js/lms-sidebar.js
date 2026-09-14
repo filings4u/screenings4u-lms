7e02c092ac4f6944307bc3c980fc6392ecf94865
filings4u <aerving1@gmail.com>
2026-09-14 13:34:14 -0500

    updates

--- assets/js/lms-sidebar.js
+++ assets/js/lms-sidebar.js
@@ -82,7 +82,7 @@
               aria-label="Screenings4u Learning Center"
             >
               <img
-                src="images/logo2.png"
+                src="images/logo.png"
                 alt="screenings4u"
                 class="lms-brand-logo"
               />
