d3fd735e1492f2d21124682419887231859376d7
filings4u <aerving1@gmail.com>
2026-09-23 11:05:24 -0500

    updates

--- assets/js/training-site-nav.js
+++ assets/js/training-site-nav.js
@@ -7,8 +7,7 @@
     host.innerHTML=`
       <div class="training-shell training-header-inner">
         <a class="training-brand" href="index.html" aria-label="screenings4u Learning Center home">
-          <img src="https://rgsrubdtljyxmnihwlah.supabase.co/storage/v1/object/public/branding/logo.png" alt="screenings4u">
-          <span>Learning Center</span>
+          <img src="images/logo.png" alt="screenings4u">
         </a>
         <nav class="training-header-nav" aria-label="Learning Center navigation">
           <a href="index.html#curriculum">Curriculum</a>
